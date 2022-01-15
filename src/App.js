829de93ae8a73808245299e012c3db0d040095b0
Rachel <rh.rachelholloway@gmail.com>
2022-01-15 16:16:35 -0500

    Added API and trying to display some data on the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+// Create state variables for API data and user queries (author/genre selection)
+
+// Create component to use user query to get data back from the API 
+
+// Catch errors and display error message if unsuccessful
+
+// Create buttons with handleClick that will call the displayQuote function 
+
+// Create component for displayQuote function to display JSX on page (data from API)
+
+// Create a button to reset the page/search for a new quote
+
+// IMPORTS
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from './Header.js';
 
 function App() {
+
+// MAKE STATEFUL VARIABLES
+const [quote, setQuote] = useState([]);
+
+  useEffect ( () => {
+    axios({
+      url: "https://quote-garden.herokuapp.com/api/v3/quotes/",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        author: "Bill Gates",
+        genre: "success"
+      }
+    }).then( (response) => {
+      console.log(response.data);
+    })
+  }, []);
+
+  const handleClick = (event) => {
+    console.log("You clicked it!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <button onClick={handleClick}>Find a Random Quote</button>
+
+      {quote.map( (individualQuote) => {
+        return (
+          <div>
+            <p>{individualQuote.data[0].quoteText}</p>
+          </div>
+        )
+      })}
+      
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) => {
+    return (
+        <header>
+            <h1>The Inspired Mind</h1>
+            <h2>Find inspirational quotes by searching for a specific author or topic.</h2>
+        </header>
+    )
+}
+
+export default Header;
