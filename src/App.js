707fa192dd2519695e126949d14d874d5afa7f30
Rachel <rh.rachelholloway@gmail.com>
2022-01-16 17:00:46 -0500

    Figured out how to get the array out of the object from the API and added stateful variables and buttons

--- src/App.js
+++ src/App.js
@@ -20,23 +20,51 @@ function App() {
 
 // MAKE STATEFUL VARIABLES
 const [quote, setQuote] = useState([]);
+const [userInput, setUserInput] = useState("");
+const [genreInput, setGenreInput] = useState("");
+const [searchTerm, setSearchTerm] = useState("");
+const [genreTerm, setGenreTerm] = useState("");
 
+// Call the API
   useEffect ( () => {
     axios({
       url: "https://quote-garden.herokuapp.com/api/v3/quotes/",
       method: "GET",
       dataResponse: "json",
       params: {
-        author: "Bill Gates",
-        genre: "success"
+        author: userInput,
+        genre: genreInput
       }
     }).then( (response) => {
-      console.log(response.data);
+      console.log(response.data.data);
+      setQuote(response.data.data);
     })
-  }, []);
+  }, [searchTerm, genreTerm]);
 
   const handleClick = (event) => {
     console.log("You clicked it!");
+  }
+
+  const handleInput = (event) => {
+    console.log("It works!", event.target.value);
+    setUserInput(event.target.value);
+  }
+
+  const handleGenreInput = (event) => {
+    console.log("How about this?", event.target.value);
+    setGenreInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("This works too!");
+    setSearchTerm(userInput);
+  }
+
+  const handleGenreSubmit = (event) => {
+    event.preventDefault();
+    console.log("Does this work?");
+    setGenreTerm(genreInput);
   }
 
   return (
@@ -45,13 +73,26 @@ const [quote, setQuote] = useState([]);
 
       <button onClick={handleClick}>Find a Random Quote</button>
 
-      {quote.map( (individualQuote) => {
+      {quote.map( (singleQuote) => {
         return (
-          <div>
-            <p>{individualQuote.data[0].quoteText}</p>
+          <div key={singleQuote._id}>
+            <p>{singleQuote.quoteText}</p>
+            <p>{singleQuote.quoteAuthor}</p>
           </div>
         )
       })}
+
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="search">Search by Author</label>
+        <input type="text" id="search" onChange={ handleInput } value={ userInput }/>
+        <button>Search</button>
+      </form>
+
+      <form onSubmit={ handleGenreSubmit }>
+        <label htmlFor="genreSearch">Search by Topic</label>
+        <input type="text" id="genreSearch" onChange={ handleGenreInput } value={ genreInput } />
+        <button>Search</button>
+      </form>
       
     </div>
   );
